ff8ab19ca05a4bd192588dd24344eb8f64d9b7da
Anant Tiwari <ananttiwari231@gmail.com>
2026-04-23 20:24:10 +0530

    updated

---
+++ Number Based Programs/Print.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Print
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int i=1;
+        while(i<=10)
+        {
+            System.out.println(i);
+
+        
+        i++;
+        }
+    }
+}
