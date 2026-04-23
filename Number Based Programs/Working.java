abc8f3c9bb8db74a881e4c1cd6cd2a893c6e81f2
Anant Tiwari <ananttiwari231@gmail.com>
2026-04-23 20:14:47 +0530

    updated

---
+++ Number Based Programs/Working.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Working
+{
+    public static void main(String args[])
+    {
+        //Scanner sc=new Scanner(System.in);
+        //init;
+        //while(Condition)
+        {
+           // System.out.println(i);
+           //update;
+        }
+
+    }
+
+}
