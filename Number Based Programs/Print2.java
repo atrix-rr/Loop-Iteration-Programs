f85504ee16af00574802f856966c35d0a2f195b9
Anant Tiwari <ananttiwari231@gmail.com>
2026-04-23 20:29:27 +0530

    updated

---
+++ Number Based Programs/Print2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Print2
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i=1;
+        while(i<=20)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
