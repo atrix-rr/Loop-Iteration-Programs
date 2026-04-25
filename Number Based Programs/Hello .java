6719cb4e02ebca997d87a42839a891b1559b8412
Anant Tiwari <ananttiwari231@gmail.com>
2026-04-25 10:35:53 +0530

    updated

---
+++ Number Based Programs/Hello .java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Hello
+{
+    public static void main(String args[])
+{
+    System.out.println("Printing hello ten times");
+    int i=1;
+    while(i<=10)
+    {
+        System.out.println("Hello");
+        i++;
+    }
+}
+}
