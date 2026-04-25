8fefaa9a56cca50657212bc1ef7e5b5acf9912d5
Anant Tiwari <ananttiwari231@gmail.com>
2026-04-25 10:40:53 +0530

    updated

---
+++ Number Based Programs/Even.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Even
+{
+    public static void main(String args[])
+    {
+        System.out.println("Printing Even no. till 50");
+        int i=2;
+        while(i<=50)
+        {
+            System.out.println(i);
+            i=i+2;
+        }
+    }
+}
