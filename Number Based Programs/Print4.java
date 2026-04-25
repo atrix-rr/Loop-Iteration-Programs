9e687f2c893c32e73552475643aa8c22cd6270d7
Anant Tiwari <ananttiwari231@gmail.com>
2026-04-25 10:31:37 +0530

    updated

---
+++ Number Based Programs/Print4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Print4
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Printing no. from 20 to 1");
+        int i=20;
+        while(i>=1)
+        {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
