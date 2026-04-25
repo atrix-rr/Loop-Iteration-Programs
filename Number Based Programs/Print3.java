b5f3b621f1b3323e131d8f60df09aa9a7c360f19
Anant Tiwari <ananttiwari231@gmail.com>
2026-04-25 10:11:25 +0530

    updated

---
+++ Number Based Programs/Print3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Print3
+{
+    public static void main(String args[])
+    {
+        System.out.println("Enter no.");
+        int i=1;
+        while(i<=20)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
