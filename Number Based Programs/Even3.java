fbc8b285f73410163c6e9133dee53aa79da02f65
Anant Tiwari <ananttiwari231@gmail.com>
2026-04-26 14:35:28 +0530

    updated

---
+++ Number Based Programs/Even3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Even3
+{
+    public static void main(String args[])
+    {
+        System.out.println("Printing even no.");
+        int i=1;
+        for( i=1;i<=50;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+                
+            }
+        }
+    }  
+    
+
+}
