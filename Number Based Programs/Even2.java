87c91c30d533ffc0682f7d84dbaf858ea601ef00
Anant Tiwari <ananttiwari231@gmail.com>
2026-04-26 14:07:58 +0530

    updated

---
+++ Number Based Programs/Even2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Even2
+{
+    public static void main(String args[])
+    {
+        System.out.println("method 2");
+        int i=1;
+        while(i<=50)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+                i++;
+                
+            
+        }
+    }
+}
