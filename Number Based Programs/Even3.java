5b42fd636fa2bc0a558127fe1d74476298cb37da
Anant Tiwari <ananttiwari231@gmail.com>
2026-04-26 14:29:07 +0530

    updated

---
+++ Number Based Programs/Even3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class 
+{
+    public static void main(String args[])
+    {
+        System.out.println("Printing using For loop");
+        int i=1;
+        for(i=1;i<=50;i=i+2)
+        {
+            System.out.println(i);
+        }
+    }
+}
