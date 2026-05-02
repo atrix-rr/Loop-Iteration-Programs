7da43fd42c7bea7067ce956658401327b420f574
Anant Tiwari <ananttiwari231@gmail.com>
2026-05-02 21:01:27 +0530

    updated

---
+++ Number Based Programs/Perfect.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Perfect
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int n=sc.nextInt();
+
+        int i,s=0;
+        for(i=1;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                s+=i;
+            }
+        }   if(s==n)
+        {
+            System.out.println("Perfect No.");
+        }
+        else{
+            System.out.println("Not ");
+        }
+    }
+}
