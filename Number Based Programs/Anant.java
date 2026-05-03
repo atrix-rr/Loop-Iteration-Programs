f44fb03da862be0f209476a53ec974e367dc238f
Anant Tiwari <ananttiwari231@gmail.com>
2026-05-03 12:17:46 +0530

    updated

---
+++ Number Based Programs/Anant.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Anant
+{
+    public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Printing My Name");
+
+    int i=1;// init
+    while(i<=1000)
+    {
+        System.out.println("My Name is Anant");
+        i++;// update
+    }
+}
+}
