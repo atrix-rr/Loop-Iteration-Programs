af8815b5c7cd21ba3451ace340d3a767321f9744
Anant Tiwari <ananttiwari231@gmail.com>
2026-05-05 16:21:37 +0530

    updated

---
+++ Pattern Based/Pattern4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Pattern4
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        int i=6;
+        for(i=6 ; i<=4 ; i++)
+        {
+            for(int j=6 ; j<=i ; j++)
+            {
+                System.out.print(j);
+            }
+        }
+        
+    }
+}
