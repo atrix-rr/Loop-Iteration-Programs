414188a142a854ef1f9e8aa377282610fbb1e64a
Anant Tiwari <ananttiwari231@gmail.com>
2026-05-05 16:08:23 +0530

    updated

--- Pattern Based/Pattern3.java
+++ Pattern Based/Pattern3.java
@@ -7,9 +7,9 @@ class Pattern3
         int i=1;
         for(i=1 ; i>=4 ; i--)
         {
-            for(int j=1 ; j<=i ; j++)
+            for(int j=1 ; j<=i ; j--)
             {
-                System.out.print(i);
+                System.out.print(j);
 
             }
             System.out.println();
