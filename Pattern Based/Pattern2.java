9477d09098cb2e6fe2c7365aaa15fcae17821c5d
Anant Tiwari <ananttiwari231@gmail.com>
2026-05-05 16:13:50 +0530

    updated

---
+++ Pattern Based/Pattern2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Pattern2
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Input");
+        int i=1;
+        for(i=1 ; i<=4 ; i++)
+        {
+            for(int j=1 ; j<=i ; j++)
+            {
+                System.out.print(i);
+            }
+
+            System.out.println();
+        }
+    }
+}
