95064b0c2e55cf771849ead45b07e9166851ef2b
Anant Tiwari <ananttiwari231@gmail.com>
2026-05-07 15:16:12 +0530

    updated

--- Pattern Based/Pattern3.java
+++ Pattern Based/Pattern3.java
@@ -4,12 +4,14 @@ class Pattern3
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter No.");
+
         int i=1;
         for(i=1 ; i>=4 ; i--)
         {
-            for(int j=1 ; j<=i ; j--)
+            for(int j=1 ; j<=i ; j++)
             {
-                System.out.print(j);
+                System.out.print(i);
 
             }
             System.out.println();
