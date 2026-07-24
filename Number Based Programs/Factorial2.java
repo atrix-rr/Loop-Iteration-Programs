c7d0e36e1c8d3d529eacb7253d6d8f4b8119b625
Anant Tiwari <ananttiwari231@gmail.com>
2026-07-24 14:22:51 +0530

    updated

---
+++ Number Based Programs/Factorial2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Factorial2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int n = sc.nextInt();
+
+        int f = 1;
+        while (n >= 1) {
+            f = f * n;
+            n--;
+        }
+        {
+
+            System.out.println("Factorial:" + f);
+
+        }
+    }
+}
